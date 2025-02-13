e30ff4366993d88c0ba21ecca4eb4e6f0f98d627
Fredric Massa <rickmass2020@gmail.com>
2025-02-13 10:15:56 -0600

    added array to keep track of videos

--- scrapper.py
+++ scrapper.py
@@ -2,11 +2,22 @@ import requests
 import re
 import json
 
-
+newest_videos = []
 
 def extract_video_titles(html):
     matches = re.findall(r'"title":\{"runs":\[\{"text":"(.*?)"\}\]', html)
     return matches
+
+def is_new_video(videos):
+    if len(newest_videos) is 0:
+        newest_videos.append[videos[2]]
+        newest_videos.append[videos[1]]
+        newest_videos.append[videos[0]]
+
+    elif videos[0] not in newest_videos:
+        newest_videos.pop(0)
+        newest_videos.append(videos[0])
+
 
 def retrieve_html(url):
     r = requests.get(url)
@@ -14,9 +25,11 @@ def retrieve_html(url):
 
 
 
+
 if __name__ == '__main__':
-    newest_videos = []
-    status, html = retrieve_html('https://youtube.com/user/penguinz0/videos')
-    titles = extract_video_titles(html)
-    print(titles)
-    print(titles[0])
+    
+
+    while True:
+        status, html = retrieve_html('https://youtube.com/user/penguinz0/videos')
+        titles = extract_video_titles(html)
+        print(titles[0])
